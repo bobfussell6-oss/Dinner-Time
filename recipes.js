93ea6c926bf753d247b4ed96fb226a0b48e04e26
bobfussell6-oss <bobfussell6@hotmail.com>
2026-09-18 22:22:30 +0100

    Create recipes.js

---
+++ recipes.js
@@ -0,0 +1,45 @@
+const recipes = [
+{
+    id: 1,
+    name: "Hidden Veg Bolognese",
+    cuisine: "Italian",
+    prepTime: 10,
+    cookTime: 25,
+    kidRating: 5,
+    healthyRating: 5,
+    ingredients: [
+        "500g beef mince",
+        "2 carrots",
+        "1 onion",
+        "300g pasta"
+    ]
+},
+{
+    id: 2,
+    name: "Creamy Chicken Pasta",
+    cuisine: "Italian",
+    prepTime: 10,
+    cookTime: 20,
+    kidRating: 5,
+    healthyRating: 4,
+    ingredients: [
+        "500g chicken breast",
+        "300g pasta",
+        "100g cream cheese"
+    ]
+},
+{
+    id: 3,
+    name: "Chicken Fajita Rice Bowls",
+    cuisine: "Mexican",
+    prepTime: 15,
+    cookTime: 15,
+    kidRating: 4,
+    healthyRating: 4,
+    ingredients: [
+        "500g chicken breast",
+        "2 peppers",
+        "250g rice"
+    ]
+}
+];
